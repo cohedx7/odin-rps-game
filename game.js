396ef9361811370231e9b31f1f43c57f0a0dcc7c
cohedx7 <kimkh87@gmail.com>
2022-03-15 15:59:40 +0100

    stuck at game() function...

--- game.js
+++ game.js
@@ -13,29 +13,62 @@ function computerPlay() {
 // console.log(computerPlay());
 
 // ask player for his choice
-/*
-function playerSelection () {
-    let playerChoice = window.prompt("What is your choice: ");
-    return playerChoice.toLowerCase();
+let playerSelection = window.prompt("What is your choice: ");
+if (playerSelection.toLowerCase() === "rock") {
+    alert("You chose Rock.");
+} else if (playerSelection.toLowerCase() === "paper") {
+    alert("You chose Paper.");
+} else {
+    alert ("You chose Scissors.");
 }
-*/
 
 const computerSelection = computerPlay();
-const playerSelection = "rock";
+// const playerSelection = "rock";
+
+let playScore = 0;
+let computerScore = 0;
+let whoWon = "";
 
 // play one round of RPS
 function playRound (playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
+        whoWon = "Nobody";
         return "You tied! Both choices are the same.";
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        whoWon = "Player";
         return "You won! Rock beats scissors.";
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        whoWon = "Player";
         return "You won! Scissors beats paper.";
     } else if (playerSelection === "paper" && computerSelection === "rock") {
+        whoWon = "Player";
         return "You won! Paper beats rock.";
     } else {
+        whoWon = "Computer";
         return `You lost! ${computerSelection} beats ${playerSelection}.`;
     }
 }
 
-console.log(playRound(playerSelection, computerSelection));
+// update score for each round...
+function updateScore () {
+    if (whoWon == "Player") {
+        playerScore++;
+        console.log("You have won " + playerScore + " games.");
+    }  else if (whoWon == "Computer") {
+        computerScore++;
+        console.log("Computer has won" + computerScore + " games.");
+    }
+}
+
+// console.log(playRound(playerSelection, computerSelection));
+
+// call playRound function 5 times and update/show score after each round..??
+function game() {
+    for (let i = 0; i < 5; i++) {
+        playRound(playerSelection[i], computerSelection[i]);
+        updateScore[i];
+        console.log(updateScore());
+    }
+}
+
+console.log(game());
