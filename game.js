e3df24d2495183334d939864e27d469863de2749
cohedx7 <kimkh87@gmail.com>
2022-03-15 17:37:14 +0100

    finished rps game project

--- game.js
+++ game.js
@@ -13,62 +13,66 @@ function computerPlay() {
 // console.log(computerPlay());
 
 // ask player for his choice
-let playerSelection = window.prompt("What is your choice: ");
-if (playerSelection.toLowerCase() === "rock") {
-    alert("You chose Rock.");
-} else if (playerSelection.toLowerCase() === "paper") {
-    alert("You chose Paper.");
-} else {
-    alert ("You chose Scissors.");
-}
-
-const computerSelection = computerPlay();
-// const playerSelection = "rock";
-
-let playScore = 0;
-let computerScore = 0;
-let whoWon = "";
-
-// play one round of RPS
-function playRound (playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        whoWon = "Nobody";
-        return "You tied! Both choices are the same.";
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        whoWon = "Player";
-        return "You won! Rock beats scissors.";
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        whoWon = "Player";
-        return "You won! Scissors beats paper.";
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        whoWon = "Player";
-        return "You won! Paper beats rock.";
+function askPlayer() {
+    const playerChoice = window.prompt("What is your choice: ");
+    if (playerChoice.toLowerCase() === "rock") {
+        alert("You chose Rock.");
+        return "rock";
+    } else if (playerChoice.toLowerCase() === "paper") {
+        alert("You chose Paper.");
+        return "paper";
     } else {
-        whoWon = "Computer";
-        return `You lost! ${computerSelection} beats ${playerSelection}.`;
+        alert ("You chose Scissors.");
+        return "scissors";
     }
 }
 
-// update score for each round...
-function updateScore () {
-    if (whoWon == "Player") {
+/* commented out and called directly into game function...
+const computerSelection = computerPlay();
+const playerSelection = askPlayer();
+*/
+
+let playerScore = 0;
+let computerScore = 0;
+
+// play one round of RPS and update scores...
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "You tied! Both choices are the same.";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
         playerScore++;
-        console.log("You have won " + playerScore + " games.");
-    }  else if (whoWon == "Computer") {
+        return "You won! Rock beats scissors.";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        playerScore++;
+        return "You won! Scissors beats paper.";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        playerScore++;
+        return "You won! Paper beats rock.";
+    } else {
         computerScore++;
-        console.log("Computer has won" + computerScore + " games.");
+        const capitalize = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+        return `You lost! ${capitalize} beats ${playerSelection}.`;
     }
 }
 
 // console.log(playRound(playerSelection, computerSelection));
 
-// call playRound function 5 times and update/show score after each round..??
+/* call playRound function 5 times and update/show score after each round
+   + display message after game is over according to end result */
 function game() {
     for (let i = 0; i < 5; i++) {
-        playRound(playerSelection[i], computerSelection[i]);
-        updateScore[i];
-        console.log(updateScore());
+        const computerSelection = computerPlay();
+        const playerSelection = askPlayer();
+        console.log(playRound(playerSelection, computerSelection));
+        console.log(`Player Score: ${playerScore}. Computer Score: ${computerScore}.`);
+    } 
+    if (playerScore > computerScore) {
+        console.log("Congrats! You beat the computer at RPS!");
+    } else if (playerScore == computerScore) {
+        console.log("You finished in a tie. Not bad!");
+    } else {
+        console.log("You lost. Better luck next time.");
     }
 }
 
-console.log(game());
+game();
