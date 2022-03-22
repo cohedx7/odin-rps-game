7e3b922805e03c26593aea39566d056c980c3e67
cohedx7 <kimkh87@gmail.com>
2022-03-22 22:29:20 +0100

    updated project - game works, possibly final version

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # odin-rps-game
 Rock Paper Scissors Project
+
+---
+
+22/03/2022:
+
+Well, I had to look at a youtube tutorial by Ania Kubów (3 ways to code Rock Paper Scissors in JavaScript) in order to figure out how to implement the UI in this RPS project-game...
+
+Overall, I'm not completely pleased/satisfied by the end result, even though it works.
+
+Not sure if I'll leave it like this or update it...
--- game.js
+++ game.js
@@ -3,18 +3,21 @@ const playerChoiceShow = document.getElementById('player-choice');
 const showResult = document.getElementById('result');
 const showPlayerScore = document.getElementById('player-score');
 const showComputerScore = document.getElementById('computer-score');
+const showAnnouncement = document.getElementById('announce');
 const possibleChoices = document.querySelectorAll('button');
 let userChoice;
 let pcChoice;
 let result;
 let playerScore = 0;
 let computerScore = 0;
+let announcement;
 
 possibleChoices.forEach(option => option.addEventListener('click', (e) => {
     userChoice = e.target.id;
-    playerChoiceShow.innerHTML = userChoice;
+    playerChoiceShow.innerHTML = userChoice.charAt(0).toUpperCase() + userChoice.slice(1);
     generateComputerChoice();
     getResult();
+    announceWinner();
 }))
 
 function generateComputerChoice() {
@@ -29,7 +32,7 @@ function generateComputerChoice() {
     if (randomNumber === 3) {
         pcChoice = "scissors";
     }
-    computerChoiceShow.innerHTML = pcChoice;
+    computerChoiceShow.innerHTML = pcChoice.charAt(0).toUpperCase() + pcChoice.slice(1);
 }
 
 function getResult() {
@@ -63,4 +66,20 @@ function getResult() {
     showResult.innerHTML = result;
     showPlayerScore.innerHTML = playerScore;
     showComputerScore.innerHTML = computerScore;
+}
+
+function announceWinner() {
+    if (playerScore == 5 && computerScore < 5) {
+        announcement = "Congratulations! You have won the game!";
+    }
+    else if (computerScore == 5 && playerScore < 5) {
+        announcement = "Damn, you lost! Better luck next time.";
+    }
+    else if (playerScore > 5 || computerScore > 5 ) {
+        announcement = "Game over. Hit refresh to start again."
+    }
+    else {
+        announcement = " ";
+    }
+    showAnnouncement.innerHTML = announcement;
 }
