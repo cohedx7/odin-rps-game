3eba3cb2c2e2b03f9e00f6d9e71da6beae6e112a
cohedx7 <kimkh87@gmail.com>
2022-03-22 18:53:54 +0100

    updating files

--- game.js
+++ game.js
@@ -1,2 +1,10 @@
-const computerChoice = document.getElementById('computer-choice');
-const playerChoice = document.getElementById('player-choice');
+const computerChoiceShow = document.getElementById('computer-choice');
+const playerChoiceShow = document.getElementById('player-choice');
+const showResult = document.getElementById('result');
+const possibleChoices = document.querySelectorAll('button');
+let userChoice;
+
+possibleChoices.forEach(option => option.addEventListener('click', (e) => {
+    userChoice = e.target.id;
+    playerChoiceShow.innerHTML = userChoice;
+}))
